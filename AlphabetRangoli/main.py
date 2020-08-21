313a709e1a7559ddefe79cc141f170a2431affd2
ZirvedaAytimur <zirvedaytimur@gmail.com>
2020-08-21 17:05:21 +0300

    Alphabet Rangoli problem added.

---
+++ AlphabetRangoli/main.py
@@ -0,0 +1,14 @@
+def print_rangoli(size):
+    # your code goes here
+    string = 'abcdefghijklmnopqrstuvwxyz'
+
+    L = []
+    for i in range(size):
+        s = "-".join(string[i:size])
+        L.append((s[::-1] + s[1:]).center(4 * size - 3, "-"))
+
+    print('\n'.join(L[:0:-1] + L))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
